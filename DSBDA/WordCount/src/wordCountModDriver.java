3067ca8ed9a970c01a2b04aab06c4c399773bf0e
Swapnil Patil <patilswapnil01sp.25@gmail.com>
2020-01-08 14:52:42 +0530

    Add files via upload

---
+++ DSBDA/WordCount/src/wordCountModDriver.java
@@ -0,0 +1,24 @@
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.util.Tool;
+
+public class wordCountModDriver implements Tool {
+
+	@Override
+	public Configuration getConf() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setConf(Configuration arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public int run(String[] arg0) throws Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
