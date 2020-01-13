e48eb057df9c809ea1ee3922d507c33517af4178
Swapnil Patil <patilswapnil01sp.25@gmail.com>
2020-01-13 23:49:52 +0530

    Add files via upload

--- System Programming/2_OnePassAssembler/2_OnePass.cpp
+++ System Programming/2_OnePassAssembler/2_OnePass.cpp
@@ -23,6 +23,9 @@ struct LiteralTable{
 	int address;
 }lit[20];
 
+struct PoolTable{
+	int id;
+}poolT[20];
 int isliteral(char *word){
 	if(word[0]==61)
 		return 1;
@@ -67,20 +70,24 @@ int main(){
 	
 	//----------------Reading the Assembly Program file--------
 	
-	fstream file; 
-	int lc=0,count,l=0,ins=0,i=0,s=0,no,symFlag=0,equ=0;
-    	string line, filename,word1,word2; 
+	fstream file,symbolFile,literalFile,incFile,poolFile,outputFile; 
+	int lc=0,count,l=0,ins=0,i=0,s=0,no,symFlag=-1,equ=0,litFlag=0,pool=0,tpool=0;
+    	string line,filename,word1,word2; 
     	char *word;
  	queue <string> q;
-    	// opening file 
-    	file.open("Assembly.txt");
+   	// opening file 
+ 	symbolFile.open("Symbol.txt",fstream::in | fstream::out);
+ 	literalFile.open("Literal.txt",fstream::in | fstream::out);
+ 	incFile.open("Incomplete.txt",fstream::in | fstream::out);
+ 	poolFile.open("Pool.txt",fstream::in | fstream::out);
+ 	outputFile.open("Output.txt",fstream::in | fstream::out );
+    file.open("Assembly.txt");
 	file.clear();	
 	file.seekg(0,ios::beg);	
-	file>>word1;
-	cout<<word1;		
+	file>>word1;		
 	if(word1=="start"){
 		it = AD.find(word1);
-		cout<<"[ AD , "<<it->second<<" ]"<<endl;
+		//cout<<"[ AD , "<<it->second<<" ]"<<endl;
 		file>>word1;
 		if(istringstream(word1)>>lc){
 			 istringstream(word1)>>lc;
@@ -104,7 +111,10 @@ int main(){
 					if(AD.count(word)){
 						it = AD.find(word);
 						cout<<"\t[ AD , "<<it->second<<" ]";
-						if(strcmp(word,"ltorg")==0){						
+						outputFile<<"\t[ AD , "<<it->second<<" ]";
+						if(strcmp(word,"ltorg")==0){
+							//pool++;
+							poolT[tpool++].id=(++pool);						
 							if(!q.empty()){
 								while(!q.empty()){
 									lit[l].literal = q.front();
@@ -120,38 +130,53 @@ int main(){
 								word = strtok(0," ,");
 								if(istringstream(word) >> no){
 									cout<<"[ C , "<<no<<" ]";
+									outputFile<<"[ C , "<<no<<" ]";
 									lc=no;
 								}	
 							}else{
 								cout<<"error";
 							}				
 						}
-						else if(strcmp(word,"equ")==0){							
-							if(count==1){
-								word = strtok(0," ,");
-								if(istringstream(word) >> no){
-									cout<<"[ C , "<<no<<" ]";
-									lc=no;
-								}	
-							}else{
-								cout<<"error";
-							}				
+						else if(strcmp(word,"end")==0){
+							//pool++;
+							poolT[tpool++].id=(++pool);						
+							if(!q.empty()){
+								while(!q.empty()){
+									lit[l].literal = q.front();
+									q.pop();
+									lit[l++].address = lc++;
+								}
+							}
 						}
 					}else if(M.count(word)){
 						it = M.find(word);
+						outputFile<<"[ "<<lc<<" ]"<<" [ IS , "<<it->second<<" ]";
 						cout<<"[ "<<lc++<<" ]"<<" [ IS , "<<it->second<<" ]";							
 					}
 					else if(DL.count(word)){
 						it = DL.find(word);
+						outputFile<<"[ "<<lc<<" ]"<<" [ DL , "<<it->second<<" ]";
 						cout<<"[ "<<lc++<<" ]"<<" [ DL , "<<it->second<<" ]";
 					}
 					else if(R.count(word)){
 						it = R.find(word);
+						outputFile<<"[ "<<it->second<<" ]";
 						cout<<"[ "<<it->second<<" ]";
 					}
 					else if(isliteral(word)){
-						q.push(word);	
+						q.push(word);
+						inc[ins].name=word;
+						inc[ins++].address=lc;
+						/*int j=0;
+						queue<string> q1 = q;
+						while(!q1.empty()){
+							if(q1.front() == word){	
+							}
+							q1.pop();
+							j++;
+						}*/	
 					}else if(istringstream(word)>>no){
+							outputFile<<"[ C , "<<no<<" ]";
 							cout<<"[ C , "<<no<<" ]";
 					}else{
 						if(count>4){
@@ -160,19 +185,37 @@ int main(){
 							if(count==2 || count==3 || count==4){
 								for (int j=0;j<=ins;j++){
 									if(inc[j].name==word){
-										symFlag=1;
+										symFlag=j;
 									}	
 								}
-								if(symFlag==0){
+								if(symFlag==-1){
 									inc[ins].name=word;
-									inc[ins++].address=lc;	
+									inc[ins].address=lc;
+									outputFile<<"[ S , "<<ins<<" ]";
+									cout<<"[ S , "<<ins++<<" ]";		
+								}else{
+									outputFile<<"[ S , "<<symFlag<<" ]";
+									cout<<"[ S , "<<symFlag<<" ]";	
 								}
-
 							}else{
-								equ=1;
 								sym[s].symbol=word;
 								sym[s].address=lc;
-								sym[s++].len=strlen(word);	
+								sym[s].len=strlen(word);
+								word = strtok(0," ,");
+								if(strcmp(word,"equ")==0){
+									outputFile<<"\t[ AD , 04 ]";
+									cout<<"\t[ AD , 04 ]";
+									word = strtok(0," ,");
+									if(istringstream(word) >> no){
+										outputFile<<"[ C , "<<no<<" ]"; 
+										cout<<"[ C , "<<no<<" ]"; 
+										sym[s].address=no;
+									}
+								}else{
+									strcpy(s1,line.c_str());
+									word = strtok(s1," ");
+								}
+								s++;	
 							}
 						}	
 						
@@ -180,6 +223,7 @@ int main(){
 					word=strtok(0," ,");
 				}
 				cout<<endl;
+				outputFile<<endl;
 				count=0;
 			}
 				 
@@ -188,30 +232,36 @@ int main(){
 		cout<<"Error, Must be Start with 'START'";
 		return 0;
 	}
-	
-	while(!q.empty()){
-		lit[l].literal = q.front();
-		q.pop();
-		lit[l++].address = lc++;
-	}
 //------------Printing all the tables----------------		
 	cout<<"\n--------------------------------- Literal Table : -----------------------------------------"<<endl;
 	int z=0;
 	while(z!=l){
-		cout<<lit[z].literal<<" "<<lit[z++].address<<endl;
+		cout<<lit[z].literal<<" "<<lit[z].address<<endl;
+		literalFile<<lit[z].literal<<" "<<lit[z++].address<<endl;
 	}
 	
 	cout<<"\n------------------------------Table of incomplete Ins : ------------------------------------"<<endl;
 	z=0;
 	while(z!=ins){
-		cout<<inc[z].name<<" "<<inc[z++].address<<endl;
+		cout<<inc[z].name<<" "<<inc[z].address<<endl;
+		incFile<<inc[z].name<<" "<<inc[z++].address<<endl;		
 	}
 
 	cout<<"\n------------------------------Table of Symbols ------------------------------------"<<endl;
 	z=0;
 	while(z!=s){
-		cout<<sym[z].symbol<<" "<<sym[z].address<<" "<<sym[z++].len<<endl;
+		cout<<sym[z].symbol<<"\t"<<sym[z].address<<"\t"<<sym[z].len<<endl;
+		symbolFile<<sym[z].symbol<<"\t"<<sym[z].address<<"\t"<<sym[z++].len<<endl;
 	}
+
+	cout<<"\n------------------------------Pool Table ------------------------------------"<<endl;
+	z=0;
+	while(z!=tpool){
+		cout<<"#"<<poolT[z].id<<endl;
+		poolFile<<"#"<<poolT[z++].id<<endl;
+	}
+	symbolFile.close();
+	literalFile.close();
 	file.close();
 	return 0; 
 }
