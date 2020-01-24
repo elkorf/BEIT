fa45afff85f351d47391528f68ea5d4d372bef4e
Swapnil Patil <patilswapnil01sp.25@gmail.com>
2020-01-24 15:01:19 +0530

    Add files via upload

---
+++ System Programming/5_Opt_Tape_Storage/OptimalTapeStorage.cpp
@@ -0,0 +1,59 @@
+#include<iostream>
+using namespace std;
+
+void getElements(int *,int);
+void dispElements(int *,int);
+void optimalTape(int *elements,int noE,int *sizeTape,int noTape);
+int main(){
+	int noE,*elements,noTape,j=0;
+	cout<<"ENTER THE NUMBER OF ELEMENTS : ";
+	cin>>noE;
+	
+	elements=new int[noE];
+	
+	getElements(elements,noE);
+	dispElements(elements,noE);
+	
+	cout<<"\nENTER THE NUMBER OF TAPES : "<<endl;
+	cin>>noTape;
+	int *sizeTape;
+	sizeTape = new int[noTape];	
+	while(j!=noTape){
+		cout<<"\nENTER THE SIZE OF TAPE "<<j<<" : ";
+		cin>>sizeTape[j++];
+	}
+	
+	optimalTape(elements,noE,sizeTape,noTape);	
+}
+
+void getElements(int *elements,int noE){
+	cout<<"ENTER THE ELEMENTS : "<<endl;
+	int i=0;	
+	while(i!=noE){
+		cout<<"ENTER "<<i<<" : ";
+		cin>>elements[i++];		
+	}
+}
+
+
+void dispElements(int *elements,int noE){
+	cout<<"ELEMENTS ARE : "<<endl;
+	int i=0;	
+	while(i!=noE){
+		cout<<elements[i++]<<"\t";		
+	}
+}
+
+void optimalTape(int *elements,int noE,int *sizeTape,int noTape){
+	int add=0,i=0,j=0;		
+	do{
+		cout<<"\n----------------Tape "<<j+1<<"-----------------"<<endl;
+		while(add<sizeTape[j]){
+			add += elements[i];
+			cout<<elements[i++]<<"\t";	
+		}
+		//add=0;		
+		j++;
+	}while(j!=noTape);	
+}
+
