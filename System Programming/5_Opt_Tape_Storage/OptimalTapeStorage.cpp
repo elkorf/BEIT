1c4754e1bfffaabb7ef37ea881db2a3f1c817356
Swapnil Patil <patilswapnil01sp.25@gmail.com>
2020-02-04 12:12:37 +0530

    Add files via upload

--- System Programming/5_Opt_Tape_Storage/OptimalTapeStorage.cpp
+++ System Programming/5_Opt_Tape_Storage/OptimalTapeStorage.cpp
@@ -1,61 +1,96 @@
 #include<iostream>
 using namespace std;
 
-void getElements(int *,int);
-void dispElements(int *,int);
-void optimalTape(int *elements,int noE,int *sizeTape,int noTape);
-int main(){
-	int noE,*elements,noTape,j=0;
-	cout<<"ENTER THE NUMBER OF ELEMENTS : ";
-	cin>>noE;
-	
-	elements=new int[noE];
-	
-	getElements(elements,noE);
-	dispElements(elements,noE);
-	
-	cout<<"\nENTER THE NUMBER OF TAPES : "<<endl;
-	cin>>noTape;
-	int *sizeTape;
-	sizeTape = new int[noTape];	
-	while(j!=noTape){
-		cout<<"\nENTER THE SIZE OF TAPE "<<j<<" : ";
-		cin>>sizeTape[j++];
+void getPrograms(int *,int);
+void dispPrograms(int *,int);
+void optimalTape(int *programs,int noE,int *sizeTape,int noTape);
+
+struct Tape{
+	int size,noE;
+	int elements[];
+};
+
+void getSizeTapes(struct Tape T[],int noTape){
+	for(int i=0;i<noTape;i++){
+		cout<<"Enter size of Tape "<<i+1<<" : ";
+		cin>>T[i].size;
 	}
-	
-	optimalTape(elements,noE,sizeTape,noTape);	
 }
 
-void getElements(int *elements,int noE){
+void optimalTape(int *programs,int noP,struct Tape T[],int noTape){	
+	int add=0,i=0,j=0,noE=0;		
+
+	while(j<noTape){
+		if(programs[i]<=T[j].size){
+			T[j].elements[noE++]=programs[i];
+			T[j].size -= programs[i++];		
+		}else{
+			T[j++].noE=noE;
+			noE=0;	
+		}
+	}	
+	
+	for(int i=0;i<noTape;i++){
+		int j=0;
+		cout<<"Tape : "<<i+1<<" elements : ";;			
+		while(j!=T[i].noE){	
+			cout<<T[i].elements[j++];
+		}	
+	}
+}
+
+int main(){
+	int noP,j=0,*programs,noTape;
+	
+	
+	cout<<"ENTER THE NUMBER OF PROGRAMS : ";
+	cin>>noP;	
+	programs=new int[noP];
+	getPrograms(programs,noP);	
+	dispPrograms(programs,noP);
+
+	cout<<"\nENTER THE NUMBER OF TAPES : "<<endl;
+	cin>>noTape;
+
+	struct Tape T[noTape];
+	getSizeTapes(T,noTape);
+	
+	optimalTape(programs,noP,T,noTape);	
+
+}
+
+void sortPrograms(int *programs,int noE){
+	for(int i=0;i<noE;i++){
+		for(int j=0;j<noE;j++){
+			if(programs[i]<programs[j]){
+				int temp=programs[i];
+				programs[i]=programs[j];
+				programs[j]=temp;				
+			}		
+		}
+	}	
+
+}
+
+void getPrograms(int *programs,int noE){
 	cout<<"ENTER THE ELEMENTS : "<<endl;
 	int i=0;	
 	while(i!=noE){
 		cout<<"ENTER "<<i<<" : ";
-		cin>>elements[i++];		
+		cin>>programs[i++];		
 	}
+	sortPrograms(programs,noE);
 }
 
 
-void dispElements(int *elements,int noE){
+
+void dispPrograms(int *programs,int noE){
 	cout<<"ELEMENTS ARE : "<<endl;
 	int i=0;	
 	while(i!=noE){
-		cout<<elements[i++]<<"\t";		
+		cout<<programs[i++]<<"\t";		
 	}
+	cout<<endl;
 }
 
-void optimalTape(int *elements,int noE,int *sizeTape,int noTape){
-	int add=0,i=0,j=0;		
-	while(j<noTape){
-		cout<<"\n----------------Tape "<<j+1<<"-----------------"<<endl;
-		cout<<"Size  : "<<sizeTape[j]<<endl;
-		add += elements[i];	
-		while(add <= sizeTape[j]){
-			cout<<elements[i++]<<"\t";			
-			add += elements[i];	
-		}
-		add=0;		
-		j++;
-	}
-}
 
